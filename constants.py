c83042e08129d31c81fe54d2e1fb4635a8eee1be
Hugo Setyadji <hugosetyadji@lifung.com>
2021-06-18 12:07:40 +0800

    refactor: change ExcelItem to use dataclass

--- constants.py
+++ constants.py
@@ -1,24 +1,10 @@
 from datetime import date
+from dataclasses import dataclass
 
 APP_VERSION = 'v0.2.12'
 DATE = date.today().strftime("%d-%b-%y")
 
 CAT_REF = "excel_categories.txt"
-DEFAULT_CATEGORIES = """[MISC]
-LIST
-ITEM LIST
-
-[CATEGORIES]
-Fresh
-Sundries
-Packaging
-Utensils
-Appliances
-Cleaning
-Stationary
-Advertising
-Utility
-"""
 DEFAULT_CATEGORIES = {
     "MISC": [
         "LIST",
@@ -38,26 +24,15 @@ DEFAULT_CATEGORIES = {
 }
 
 
+@dataclass
 class ExcelItem:
     """Basic class to hold item data"""
-    def __init__(
-        self,
-        name=None,
-        vendor=None,
-        brand=None,
-        quantity=None,
-        unit=None,
-        cost=None,
-        isi=None,
-        isi_unit=None,
-        category=None
-    ):
-        self.name = name
-        self.vendor = vendor
-        self.brand = brand
-        self.quantity = quantity
-        self.unit = unit
-        self.cost = cost
-        self.isi = isi
-        self.isi_unit = isi_unit
-        self.category = category
+    name: str
+    vendor: str
+    brand: str
+    quantity: int
+    unit: str
+    cost: int
+    isi: int
+    isi_unit: str
+    category: str
