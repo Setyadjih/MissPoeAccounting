28dc1184e2833afae2f12306ae09a31081a21cf1
Hugo Setyadji <setyadjih@gmail.com>
2023-07-14 23:15:39 +0800

    chore: increment version

--- core/constants.py
+++ core/constants.py
@@ -2,7 +2,7 @@ from datetime import date
 from dataclasses import dataclass
 from enum import Enum
 
-APP_VERSION = "v1.3.4"
+APP_VERSION = "v1.4.0"
 DATE = date.today().strftime("%d-%b-%y")
 LOGGER_NAME = "automator.log"
 
