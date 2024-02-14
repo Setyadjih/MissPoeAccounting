056fbb6d87451fad2f4785e48383f5be9d6fa95d
Hugo Setyadji <setyadjih@gmail.com>
2024-02-14 23:17:23 +0800

    chore: set major version

--- core/constants.py
+++ core/constants.py
@@ -2,7 +2,7 @@ from datetime import date
 from dataclasses import dataclass
 from enum import Enum
 
-APP_VERSION = "v1.4.8"
+APP_VERSION = "v2.0.0"
 DATE = date.today().strftime("%d-%b-%y")
 LOGGER_NAME = "automator.log"
 
